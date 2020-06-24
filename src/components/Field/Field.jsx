804c5b67fe4f866451fed6a80784bce1d78f27c3
Pelevina Anastasia <pelevina.2000@mail.ru>
2020-06-24 22:04:27 +0500

    made Field component functional

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { DropTarget } from 'react-dnd';
+import React, { useState } from 'react';
+import { useDrop } from 'react-dnd';
 import ItemTypes from '../ItemTypes';
 import DragObject from '../DragObject/DragObject';
 import update from 'immutability-helper';
@@ -16,126 +16,68 @@ const styles = {
   backgroundColor: "#f3f3f3"
 };
 
-class Field extends React.Component {
-  constructor(props) {
-    super(props);
-    this.initialState = {
-      boxes: config.nodes,
-      lines: config.edges,
-      showModal: false
-    };
-
-    this.state = this.initialState;
-  }
-
-  restartGame = () => {
-    this.setState(this.initialState);
-  };
-
-  multifyVectors(vector1, vector2) {
-    return vector1.x * vector2.y - vector1.y * vector2.x;
-  }
-
-  areVectorsCrossed(vector1, vector2, vector3, vector4) {
-    const v1 = this.multifyVectors({ x: vector4.left - vector3.left, y: vector4.top - vector3.top },
-      { x: vector1.left - vector3.left, y: vector1.top - vector3.top });
-    const v2 = this.multifyVectors({ x: vector4.left - vector3.left, y: vector4.top - vector3.top },
-      { x: vector2.left - vector3.left, y: vector2.top - vector3.top });
-    const v3 = this.multifyVectors({ x: vector2.left - vector1.left, y: vector2.top - vector1.top },
-      { x: vector3.left - vector1.left, y: vector3.top - vector1.top });
-    const v4 = this.multifyVectors({ x: vector2.left - vector1.left, y: vector2.top - vector1.top },
-      { x: vector4.left - vector1.left, y: vector4.top - vector1.top });
-
-    return v1 * v2 < 0 && v3 * v4 < 0;
-  }
-
-  render() {
-    const { connectDropTarget } = this.props;
-    const { boxes, lines } = this.state;
-
-    return connectDropTarget(
-      <div>
-        <Modal showModal={this.state.showModal} restartGame={this.restartGame} />
-
-        <div style={styles}>
-          <svg style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            height: '600px',
-            width: '600px'
-          }}>
-            {lines.map(([a, b]) => {
-              return (
-                <line x1={boxes[a].left + 20}
-                      y1={boxes[a].top + 20}
-                      x2={boxes[b].left + 20}
-                      y2={boxes[b].top + 20}
-                      style={{
-                        stroke: '#C4CCF9',
-                        strokeWidth: 5,
-                        opacity: '0.8'
-                      }}
-                />
-              );
-            })}
-          </svg>
-
-          {Object.keys(boxes).map(key => {
-            const { left, top } = boxes[key];
-
-            return (
-              <DragObject key={key} id={key} left={left} top={top}>
-                {key}
-              </DragObject>
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
-
-  moveBox(id, left, top) {
-    this.setState(
-      update(this.state, {
-        boxes: {
-          [id]: {
-            $merge: { left, top },
-          },
-        },
-      }),
-    );
-  }
+function multifyVectors(vector1, vector2) {
+  return vector1.x * vector2.y - vector1.y * vector2.x;
 }
 
-export default DropTarget(
-  ItemTypes.BOX,
-  {
-    drop(props, monitor, component) {
-      const { lines, boxes } = component.state;
+function areVectorsCrossed(vector1, vector2, vector3, vector4) {
+  const v1 = multifyVectors({ x: vector4.left - vector3.left, y: vector4.top - vector3.top },
+    { x: vector1.left - vector3.left, y: vector1.top - vector3.top });
+  const v2 = multifyVectors({ x: vector4.left - vector3.left, y: vector4.top - vector3.top },
+    { x: vector2.left - vector3.left, y: vector2.top - vector3.top });
+  const v3 = multifyVectors({ x: vector2.left - vector1.left, y: vector2.top - vector1.top },
+    { x: vector3.left - vector1.left, y: vector3.top - vector1.top });
+  const v4 = multifyVectors({ x: vector2.left - vector1.left, y: vector2.top - vector1.top },
+    { x: vector4.left - vector1.left, y: vector4.top - vector1.top });
+
+  return v1 * v2 < 0 && v3 * v4 < 0;
+}
+
+function Field({ connectDropTarget }) {
+  const [ nodes, setNodes ] = useState(config.nodes);
+  const [ edges, setEdges ] = useState(config.edges);
+  const [ showModal, setShowModal ] = useState(false);
+
+  const restartGame = () => {
+    setNodes(config.nodes);
+    setEdges(config.edges);
+    setShowModal(false);
+  };
+
+  const moveNode = (id, left, top) => {
+    setNodes(update(edges, {
+      [id]: {
+        $merge: { left, top },
+      }
+    }));
+  };
+
+  const [, drop] = useDrop({
+    accept: ItemTypes.BOX,
+    drop(props, monitor) {
       const checked = [];
 
-      for (let line1 of lines) {
-        for (let line2 of lines) {
+      for (let line1 of edges) {
+        for (let line2 of edges) {
           if (checked.includes(line2)) {
             continue;
           }
 
-          if (component.areVectorsCrossed(boxes[line1[0]], boxes[line1[1]], boxes[line2[0]], boxes[line2[1]])) {
-
-            if (component.state.showModal) {
-              component.setState({...component.state, showModal: false});
+          if (areVectorsCrossed(edges[line1[0]], edges[line1[1]], edges[line2[0]], edges[line2[1]])) {
+            if (showModal) {
+              setShowModal(false);
             }
-            return
+
+            return;
           }
         }
 
         checked.push(line1);
       }
 
-      component.setState({...component.state, showModal: true});
+      setShowModal(true);
     },
-    hover(props, monitor, component) {
+    hover(props, monitor) {
       const item = monitor.getItem();
       const delta = monitor.getDifferenceFromInitialOffset();
       let left = Math.round(item.left + delta.x);
@@ -152,10 +94,50 @@ export default DropTarget(
       if (left < 0) {
         left = 0;
       }
-      component.moveBox(item.id, left, top);
+      moveNode(item.id, left, top);
     }
-  },
-  connect => ({
-    connectDropTarget: connect.dropTarget(),
-  }),
-)(Field)
+  });
+
+  return connectDropTarget(
+    <div>
+      <Modal showModal={showModal} restartGame={restartGame} />
+
+      <div ref={drop} style={styles}>
+        <svg style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          height: '600px',
+          width: '600px'
+        }}>
+          {edges.map(([a, b]) => {
+            return (
+              <line x1={nodes[a].left + 20}
+                    y1={nodes[a].top + 20}
+                    x2={nodes[b].left + 20}
+                    y2={nodes[b].top + 20}
+                    style={{
+                      stroke: '#C4CCF9',
+                      strokeWidth: 5,
+                      opacity: '0.8'
+                    }}
+              />
+            );
+          })}
+        </svg>
+
+        {Object.keys(nodes).map(key => {
+          const { left, top } = nodes[key];
+
+          return (
+            <DragObject key={key} id={key} left={left} top={top}>
+              {key}
+            </DragObject>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Field;
