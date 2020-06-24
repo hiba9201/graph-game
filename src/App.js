3e16e1b628f0b9ac9829f7e308a95fce8d995401
Pelevina Anastasia <pelevina.2000@mail.ru>
2020-06-25 00:21:02 +0500

    * fixed colors * made Modal functional component * made App functional component * moved Modal styles to css file

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import Field from "./components/Field/Field";
 import TouchBackend from 'react-dnd-touch-backend'
 import { DndProvider } from "react-dnd";
 
-class App extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <DndProvider backend={TouchBackend} options={{
-          enableMouseEvents: true
-        }}>
-          <Field />
-        </DndProvider>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <DndProvider backend={TouchBackend} options={{
+        enableMouseEvents: true
+      }}>
+        <Field />
+      </DndProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,33 +1,15 @@
 import React from 'react';
 
-function Modal(props) {
-  const { showModal, restartGame } = props;
+import './Modal.css';
 
+function Modal({ showModal, restartGame }) {
   return (
-    <div style={{
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.8)',
-      display: showModal ? 'block' : 'none',
-      position: 'fixed',
-      zIndex: 10,
-      top: 0,
-      left: 0,
-    }}>
-      <div style={{
-        margin: '200px auto',
-        width: 'max-content',
-        padding: '40px 50px',
-        backgroundColor: '#fff',
-        textAlign: 'center'
-      }}>
-        <h2>Вы выиграли!</h2>
-        <button style={{
-          borderRadius: 0,
-          padding: '10px 20px',
-          backgroundColor: '#F0F0F0',
-          fontSize: '18px'
-        }} onClick={restartGame}>Начать заново</button>
+    <div className={`modal-background ${showModal && 'modal-background_show-modal'}`}>
+      <div className="modal">
+        <h2 className="modal__header">Вы выиграли!</h2>
+        <button className="modal__button" onClick={restartGame}>
+          Начать заново
+        </button>
       </div>
     </div>
   );
